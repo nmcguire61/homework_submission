4b8c398cf348ca3da92f605e09cc30944ecceeb0
NeilMcGuire <nmcguire61@gmail.com>
2015-11-02 10:21:20 +0000

    post homework review

--- cookbook/db/migrate/20151030154050_add_ingredients_id_column_to_recipe.rb
+++
@@ -1,5 +0,0 @@
-class AddIngredientsIdColumnToRecipe < ActiveRecord::Migration
-  def change
-    add_column :recipes, :ingredients_id, :integer
-  end
-end
--- cookbook/db/schema.rb
+++ cookbook/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151030154050) do
+ActiveRecord::Schema.define(version: 20151030144058) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -33,10 +33,9 @@ ActiveRecord::Schema.define(version: 20151030154050) do
   create_table "recipes", force: :cascade do |t|
     t.string   "name"
     t.string   "method"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.integer  "category_id"
-    t.integer  "ingredients_id"
   end
 
 end
